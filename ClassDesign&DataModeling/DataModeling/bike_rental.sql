1c6706b8fe4f35cf9febe26a56787b1cbca9e6ca
phuc180155 <phucnguyenphi2810@gmail.com>
2021-12-07 01:05:19 +0700

    fix fields in some entity

--- ClassDesign&DataModeling/DataModeling/bike_rental.sql
+++ ClassDesign&DataModeling/DataModeling/bike_rental.sql
@@ -2,101 +2,117 @@ create database if not exists bike_rental;
 use bike_rental;
 SET time_zone='+07:00';
 
-drop table if exists bike_rental.`rent`;
-drop table if exists bike_rental.`transaction`;
+drop table if exists bike_rental.transaction;
+drop table if exists bike_rental.rent;
+drop table if exists bike_rental.user;
 drop table if exists bike_rental.bike;
-drop table if exists bike_rental.`user`;
 drop table if exists bike_rental.category;
-drop table if exists bike_rental.station;
+drop table if exists bike_rental.dock;
 
-create table if not exists bike_rental.station (
-	stationId int not null auto_increment,
-    station_name varchar(255),
-    image_path varchar(512),
-    address varchar(512),
-    constraint pk_station primary key(stationId)
+create table if not exists bike_rental.user(
+	userId int not null auto_increment,
+    userName varchar(255) not null,
+    constraint pk_user primary key(userId)
+);
+
+create table if not exists bike_rental.dock (
+	dockId int not null auto_increment,
+    dockName varchar(255) not null,
+    address varchar(255) not null,
+    dockArea float not null,
+    availableBikes int default 0,
+    emptyDockingPoints int default 0,
+    distance float,
+    walkingTime float,
+    imagePath varchar(512),
+    constraint pk_dock primary key(dockId)
 );
 
 create table if not exists bike_rental.category(
 	categoryId int not null auto_increment,
-	`name` varchar(255),
-	`description` varchar(255),
-    cost_per_hour float,
-    n_pedals int,
-    n_seats int,
-    constraint pk_category primary key(categoryId) 
+	categoryName varchar(255) not null,
+	categoryDescription varchar(255) not null,
+    bikeValue float not null,
+    costRatio float not null,
+    constraint pk_category primary key(categoryId)
 );
 
 create table if not exists bike_rental.bike(
 	bikeId int not null auto_increment,
-    bike_name varchar(255),
-    stationId int,
+    bikeName varchar(255) not null,
+    licensePlate varchar(255) not null,
     pin float,
     status boolean,
-    categoryId int,
+    initCost float default 10,
+    costPerQuarterHour float default 3,
+    dockId int not null,
+    categoryId int not null,
     constraint pk_bike primary key(bikeId),
-    foreign key (stationId) references bike_rental.station(stationId),
-    foreign key (categoryId) references bike_rental.category(categoryId)
+    constraint fk_bike_dock foreign key (dockId) references bike_rental.dock(dockId),
+    constraint fk_bike_category foreign key (categoryId) references bike_rental.category(categoryId)
 );
 
-
-create table if not exists bike_rental.`user`(
-	userId int not null auto_increment,
-    `name` varchar(255),
-    constraint pk_user primary key(userId)
-);
-
-create table if not exists bike_rental.`transaction`(
-	transactionId int not null auto_increment,
-    userId int ,
-    total_payment float,
-    `time` timestamp,
-    bike_name varchar(255),
-    rented_duration float,
-    constraint pk_transaction primary key(transactionId),
-    foreign key (userId) references bike_rental.`user`(userId)
-);
-
-create table if not exists bike_rental.`rent`(
+create table if not exists bike_rental.rent(
 	rentId int not null auto_increment,
-    userId int,
-    bikeId int,
-    start_time timestamp default current_timestamp,
-    end_time timestamp,
+    startTime timestamp default current_timestamp,
+    endTime timestamp,
     debit int,
+    userId int not null,
+    bikeId int not null,
     constraint pk_rent primary key(rentId),
     foreign key (userId) references bike_rental.user(userId),
     foreign key (bikeId) references bike_rental.bike(bikeId)
 );
- 
--- insert data
-insert into bike_rental.station(station_name, address)
-values ('Bach Khoa', 'So 1 Dai Co Viet'), ('Ngoai Thuong', 'Chua Lang'), ('Kinh Te Quoc Dan', 'Tran Dai Nghia');
 
-insert into bike_rental.category(name, description, cost_per_hour, n_pedals, n_seats)
-values (n'Xe đạp đơn', n'1 chỗ đạp, 1 chỗ ngồi sau', 10, 1, 1),
-		(n'Xe đạp đôi', n'2 chỗ đạp, 1 chỗ ngồi sau', 15, 2, 1),
-        (n'Xe đạp điện', n'1 chỗ đạp, 1 chỗ ngồi sau, xe có thể chạy bằng điện', 15, 1, 1);
-        
-insert into bike_rental.bike(bike_name, stationId, pin, status, categoryId)
-values('No_001', 1, null, 0, 1),
-		('No_002', 1, null, 0, 2),
-        ('No_003', 1, null, 1, 1),
-        ('No_004', 1, 0.5, 1, 3),
-        ('No_005', 2, null, 1, 1),
-        ('No_006', 2, null, 1, 2),
-        ('No_007', 2, null, 1, 1),
-        ('No_008', 2, 0.5, 1, 3),
-        ('No_009', 3, null, 1, 1),
-        ('No_010', 3, null, 1, 2),
-        ('No_011', 3, null, 1, 2),
-        ('No_012', 3, 0.5, 1, 3)
-;
+create table if not exists bike_rental.transaction(
+	transactionId int not null auto_increment,
+    totalCost float,
+    content varchar(255),
+    rentedDuration float,
+    modifyAt timestamp default current_timestamp,
+    userId int not null,
+    rentId int not null,
+    bikeId int not null,
+    constraint pk_transaction primary key(transactionId),
+    constraint fk_transaction_user foreign key (userId) references bike_rental.user(userId),
+    constraint fk_transaction_rent foreign key (rentId) references bike_rental.rent(rentId),
+    constraint fk_transaction_bike foreign key (bikeId) references bike_rental.bike(bikeId)
+);
 
-insert into bike_rental.user(name)
-values ('admin');
+--
+--    seed data
+--
+insert into bike_rental.user(userName)
+values  ('admin');
 
-insert into bike_rental.rent(userId, bikeId, start_time, end_time, debit)
-values (1, 1, '2020-12-10 00:00:01', '2020-12-11 06:00:01', 5),
-		(1, 2, now(), null, 5);
+insert into bike_rental.category(categoryName, categoryDescription, bikeValue, costRatio)
+values  (n'Xe đạp đơn thường', n'1 yên/bàn đạp và 1 ghế ngồi phía sau', 400, 1.0),
+	    (n'Xe đạp đôi', n'có 2 yên/bàn đạp và 1 ghế ngồi phía sau', 550, 1.5),
+        (n'Xe đạp đơn điện', n'1 yên/bàn đạp và 1 ghế ngồi phía sau, có motor điện giúp đạp xe nhanh hơn', 700, 1.5);
 
+insert into bike_rental.dock(dockName, address, dockArea, availableBikes, emptyDockingPoints)
+values  (n'Bách Khoa', n'Hai Bà Trưng - Hà Nội', 400, 10, 15),
+        (n'Bạch Mai', n'Thanh Xuân - Hà Nội', 550, 12, 20),
+        (n'Vincom', n'Cầu Giấy - Hà Nội', 1000, 50, 60),
+        (n'Vinpearl', n'Thạch Hải - Hà Tĩnh', 650, 15, 16),
+        (n'Quang Vinh', n'Thành phố Hà Tĩnh - Hà Tĩnh', 500, 13, 10),
+        (n'Lê Trọng Tấn', n'Thanh Xuân - Hà Nội', 700, 18, 15);
+
+insert into bike_rental.bike(bikeName, licensePlate, initCost, costPerQuarterHour, dockId, categoryId)
+values  ('No_001', '38MH_001', 10, 3, 1, 1),
+        ('No_002', '38MH_002', 10, 3, 2, 1),
+        ('No_003', '38MH_003', 10, 3, 3, 1),
+        ('No_004', '38MH_004', 10, 3, 4, 1),
+        ('No_005', '38MH_005', 10, 3, 5, 1),
+        ('No_006', '38MH_006', 10, 3, 1, 2),
+        ('No_007', '38MH_007', 10, 3, 1, 3),
+        ('No_008', '38MH_008', 10, 3, 2, 2),
+        ('No_009', '38MH_009', 10, 3, 3, 2),
+        ('No_010', '38MH_010', 10, 3, 3, 3);
+
+insert into bike_rental.rent(startTime, endTime, debit, userId, bikeId)
+values  (now(), '2022-10-01 07:00:00', 5, 1, 1),
+	    (now(), '2022-11-01 07:00:00', 5, 1, 2),
+        (now(), '2022-12-01 07:00:00', 5, 1, 3),
+        (now(), '2022-09-01 07:00:00', 5, 1, 4),
+        (now(), '2022-08-01 07:00:00', 5, 1, 5);
